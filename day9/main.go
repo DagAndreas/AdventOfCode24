3e387b5308c0b037dcc83e664c67c9442be24174
Dag Andreas Foss Folvell <dag.folvell.ext@siemens.com>
2024-12-09 16:56:47 +0100

    day 9

--- day9/main.go
+++ day9/main.go
@@ -8,7 +8,7 @@ import (
 )
 
 func main() {
-	filepath := "input.txt"
+	filepath := "test.txt"
 	file, err := os.Open(filepath)
 	if err != nil {
 		log.Fatal(err)
@@ -17,11 +17,12 @@ func main() {
 	scanner := bufio.NewScanner(file)
 	scanner.Scan()
 	disk_ints := format_disk_map(scanner.Text())
+	fmt.Println("disk_ints: ", disk_ints)
 
-	disk_shifted := left_shift_disk(disk_ints)
-
-	ans := find_checksum(disk_shifted)
-	fmt.Println("result: ", ans)
+	disk_shifted := left_shift_disk_part2(disk_ints)
+	fmt.Println(disk_shifted)
+	// ans := find_checksum(disk_shifted)
+	// fmt.Println("result: ", ans)
 }
 
 func find_checksum(disk []int) int64 {
@@ -38,7 +39,7 @@ func find_checksum(disk []int) int64 {
 	return sum
 }
 
-func left_shift_disk(disk []int) []int {
+func left_shift_disk_part1(disk []int) []int {
 	left_pointer := 0
 	right_pointer := len(disk) - 1
 
@@ -65,6 +66,97 @@ func left_shift_disk(disk []int) []int {
 	return disk
 }
 
+func left_shift_disk_part2(disk []int) []int {
+	// left_pointer := 0
+	// right_pointer := len(disk) - 1
+
+	// look at the end and find a chunk of data.
+
+	for i := len(disk) - 1; i > 0; i-- {
+
+		// backwards indexing
+		fmt.Println("backwards: ", disk[i])
+		cell_value := disk[i]
+
+		empty_cell := -1
+		if cell_value == empty_cell {
+			fmt.Println("empty cell")
+			continue
+		}
+
+		// we found a chunk of memory
+		// let's count how big the memory is
+
+		// look_for_next_value := true
+		memory_size_chunk := 1 // we already know this is one
+		for j := i - 1; i > 0; j-- {
+			if j == 1 {
+				break
+			}
+			previous_cell := disk[j]
+			fmt.Println("checking for chunk: ", previous_cell)
+			if previous_cell != cell_value {
+				break
+			}
+			memory_size_chunk++
+		}
+
+		fmt.Println("found a chunk of ", cell_value, " that is ", memory_size_chunk, "long")
+		first_index_of_memory := i - memory_size_chunk
+		fmt.Println("chunk begins at i=", first_index_of_memory)
+
+		// now we need to look forward to find a continous memory addess for it.
+		for j := 0; j < first_index_of_memory; j++ {
+
+			not_empty_cell := disk[j] != empty_cell
+			if not_empty_cell {
+				fmt.Println(j, " is not empty")
+				continue
+			}
+
+			// we found an empty cell. Let's look ahead and see if it has enough memory for the whole chunk
+			// begin on j.
+			// look ahead memorysize chunk times
+			fmt.Println("Before looking for space")
+			found_enough_space := true
+			for x := j; x < memory_size_chunk; x++ {
+				if disk[x] != empty_cell {
+					fmt.Println("Not enough space for the chunk at ", j)
+					fmt.Println("Need ", memory_size_chunk, " but found only ", x)
+					found_enough_space = false
+					break
+				}
+			}
+			fmt.Println("after looking for space")
+			if !found_enough_space {
+				fmt.Println("not enough space from index ", j)
+				continue
+			}
+
+			// lets copy the cell value into the next memorychunk spots
+			for x := j; x < memory_size_chunk+j; x++ {
+				fmt.Println("copying value. Clearing ", disk[x], " over to index ", x, " with value ", cell_value)
+				disk[x] = cell_value
+			}
+
+			// lets clear the memory chunk
+			for x := i - memory_size_chunk; x < i; x-- {
+				fmt.Println("x:", x, " : ", i, " mem_size_chunk:", memory_size_chunk)
+				fmt.Println("setting value ", disk[x], " to -1 ")
+				disk[x] = -1
+			}
+
+			// now lets shift i down mem_size
+			i -= memory_size_chunk
+			break
+
+		}
+
+	}
+
+	return disk
+}
+
 func format_disk_map(s string) []int {
 	even := true
 	ascii_ofset := 48
@@ -83,7 +175,6 @@ func format_disk_map(s string) []int {
 			for range value {
 				intslice = append(intslice, id)
 			}
-			println("id is now", id)
 			id++
 		} else {
 			for range value {
