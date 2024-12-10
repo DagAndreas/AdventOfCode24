45cd9081b953bea9cda089a5567fc134dd5c15d7
Dag Andreas Foss Folvell <dag.folvell.ext@siemens.com>
2024-12-10 13:42:29 +0100

    completed day 10

--- day10/main.go
+++ day10/main.go
@@ -26,7 +26,6 @@ func main() {
 		splt := strings.Split(nums_text, "")
 		var int_list []int
 		for i := range splt {
-			fmt.Println(splt[i])
 			num, _ := strconv.ParseInt(splt[i], 10, 32)
 			int_list = append(int_list, int(num))
 
@@ -35,18 +34,20 @@ func main() {
 
 		index++
 	}
-
-	result := process_map(top_map)
-	fmt.Println("result: ", result)
+	var paths int
+	result, paths := process_map(top_map)
+	fmt.Println("result (star1): ", result)
+	fmt.Println("paths: (star2)", paths)
 
 }
 
-func process_map(top_map [][]int) int {
+func process_map(top_map [][]int) (int, int) {
 
 	// first lets iterate and find a '0'
 	start_tile := 0
 
 	sum := 0
+	met := 0
 	for row := range top_map {
 		for column := range top_map[row] {
 			curr_tile := top_map[row][column]
@@ -54,36 +55,30 @@ func process_map(top_map [][]int) int {
 				continue
 			}
 
-			fmt.Println("starting traversasl from ", row, ",", column)
 			results := make(map[string]bool)
-			results = traverse_map_from_pos(top_map, row, column, 0, results)
-			fmt.Println(results)
-			// convert results to a set
+			var found int
+			results, found = traverse_map_from_pos(top_map, row, column, 0, results)
+			met += found
 
+			// convert results to a set
 			sum += len(results)
 			// count the length of set and add to sum
 		}
 	}
 
-	return sum
+	return sum, met
 
 }
 
-func traverse_map_from_pos(top_map [][]int, row int, column int, current int, found map[string]bool) map[string]bool {
+func traverse_map_from_pos(top_map [][]int, row int, column int, current int, found map[string]bool) (map[string]bool, int) {
 	end_value := 9
-	fmt.Println("traversing from ", row, ", ", column, " with current ", current)
 
 	if current == end_value {
-		fmt.Println("found the 9! Returning")
-
-		// a := ""
-		// a = a + string(row) + "," + string(column)
+		// create key
 		a := strconv.Itoa(row) + "," + strconv.Itoa(column)
-		fmt.Println("a=", a)
 		found[a] = true
-		fmt.Println("added to map")
 
-		return found
+		return found, 1
 	}
 
 	// limits for indexing
@@ -92,31 +87,30 @@ func traverse_map_from_pos(top_map [][]int, row int, column int, current int, fo
 	column_left_limit := 0
 	column_right_limit := len(top_map[0]) - 1
 
-	// found_paths := 0
+	found_paths := 0
 
+	var met int
 	if check_cell(row-1, row_top_limit, true) && top_map[row-1][column] == current+1 {
-		fmt.Println("checking above in ", row, ", ", column)
-		found = traverse_map_from_pos(top_map, row-1, column, current+1, found)
+		found, met = traverse_map_from_pos(top_map, row-1, column, current+1, found)
+		found_paths += met
 	}
 
 	if check_cell(row+1, row_bottom_limit, false) && top_map[row+1][column] == current+1 {
-		fmt.Println("checking below in ", row, ", ", column)
-		found = traverse_map_from_pos(top_map, row+1, column, current+1, found)
+		found, met = traverse_map_from_pos(top_map, row+1, column, current+1, found)
+		found_paths += met
 	}
 
 	if check_cell(column-1, column_left_limit, true) && top_map[row][column-1] == current+1 {
-		fmt.Println("checking left in ", row, ", ", column)
-		found = traverse_map_from_pos(top_map, row, column-1, current+1, found)
+		found, met = traverse_map_from_pos(top_map, row, column-1, current+1, found)
+		found_paths += met
 	}
 
 	if check_cell(column+1, column_right_limit, false) && top_map[row][column+1] == current+1 {
-		fmt.Println("checking right in ", row, ", ", column)
-		found = traverse_map_from_pos(top_map, row, column+1, current+1, found)
-
+		found, met = traverse_map_from_pos(top_map, row, column+1, current+1, found)
+		found_paths += met
 	}
 
-	fmt.Println("found len found:", len(found), " paths")
-	return found
+	return found, found_paths
 
 }
 
