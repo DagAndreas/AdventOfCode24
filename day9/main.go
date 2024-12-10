a4a1b5e669694925f56abe45653f85b216bc9363
Dag Andreas Foss Folvell <dag.folvell.ext@siemens.com>
2024-12-10 10:11:47 +0100

    day 2 rewrite

--- day9/main.go
+++ day9/main.go
@@ -21,8 +21,8 @@ func main() {
 
 	disk_shifted := left_shift_disk_part2(disk_ints)
 	fmt.Println(disk_shifted)
-	// ans := find_checksum(disk_shifted)
-	// fmt.Println("result: ", ans)
+	ans := find_checksum(disk_shifted)
+	fmt.Println("result: ", ans)
 }
 
 func find_checksum(disk []int) int64 {
@@ -66,17 +66,38 @@ func left_shift_disk_part1(disk []int) []int {
 	return disk
 }
 
+func find_biggest_id_start(disk []int) int {
+	empty_cell := -1
+	biggest := 0
+	for i := len(disk) - 1; i > 0; i-- {
+		if disk[i] != empty_cell {
+			biggest = disk[i]
+			break
+		}
+	}
+	return biggest
+}
+
 func left_shift_disk_part2(disk []int) []int {
 	// left_pointer := 0
 	// right_pointer := len(disk) - 1
 
 	// look at the end and find a chunk of data.
 
+	// to stop pushing the bigger parts backwards. we need to keep track of the largest id we've parsed and stop moving any id under it.
+	// we can do this because the id's are in asc order.
+	biggest_or_last_moved := find_biggest_id_start(disk) + 1
+
 	for i := len(disk) - 1; i > 0; i-- {
 
 		// backwards indexing
-		fmt.Println("backwards: ", disk[i])
+		fmt.Println("backwards: ", disk[i], " i=", i)
 		cell_value := disk[i]
+
+		if cell_value >= biggest_or_last_moved {
+			fmt.Println("Cellvalue is the biggest (", biggest_or_last_moved, "). Let's not move it again.")
+			continue
+		}
 
 		empty_cell := -1
 		if cell_value == empty_cell {
@@ -88,29 +109,35 @@ func left_shift_disk_part2(disk []int) []int {
 		// let's count how big the memory is
 
 		// look_for_next_value := true
-		memory_size_chunk := 1 // we already know this is one
-		for j := i - 1; i > 0; j-- {
-			if j == 1 {
-				break
+		chunk_sizes := 0
+		for j := i; j > 0; j-- {
+			fmt.Println("checking for size. j=", j)
+			if j == 0 && disk[j] != empty_cell {
+				fmt.Println("j reached bottom. breaking")
+				return disk
 			}
+
 			previous_cell := disk[j]
 			fmt.Println("checking for chunk: ", previous_cell)
+			fmt.Println("prev:", previous_cell, "vs cell:", cell_value)
 			if previous_cell != cell_value {
 				break
 			}
-			memory_size_chunk++
+
+			fmt.Println("increasing chunksize to : ", chunk_sizes+1)
+			chunk_sizes++
 		}
 
-		fmt.Println("found a chunk of ", cell_value, " that is ", memory_size_chunk, "long")
-		first_index_of_memory := i - memory_size_chunk
+		fmt.Println("found a chunk of ", cell_value, " that is ", chunk_sizes, "long")
+		first_index_of_memory := i - chunk_sizes + 1
 		fmt.Println("chunk begins at i=", first_index_of_memory)
 
 		// now we need to look forward to find a continous memory addess for it.
 		for j := 0; j < first_index_of_memory; j++ {
-
+			fmt.Println("starting from the bottom. j=", j)
 			not_empty_cell := disk[j] != empty_cell
 			if not_empty_cell {
-				fmt.Println(j, " is not empty")
+				fmt.Println(disk[j], " is not empty")
 				continue
 			}
 
@@ -119,13 +146,17 @@ func left_shift_disk_part2(disk []int) []int {
 			// look ahead memorysize chunk times
 			fmt.Println("Before looking for space")
 			found_enough_space := true
-			for x := j; x < memory_size_chunk; x++ {
+			fmt.Println("checking for enough empty cells from ", j)
+			for x := j; x < chunk_sizes; x++ {
+				fmt.Println("x=", x)
 				if disk[x] != empty_cell {
 					fmt.Println("Not enough space for the chunk at ", j)
-					fmt.Println("Need ", memory_size_chunk, " but found only ", x)
+					fmt.Println("Need ", chunk_sizes, " but found only ", x)
 					found_enough_space = false
 					break
 				}
+				fmt.Println("nice. x was empty. Lets check next if we need more. x:", x, " and chunk_indexes: ", chunk_sizes)
+
 			}
 			fmt.Println("after looking for space")
 			if !found_enough_space {
@@ -133,24 +164,32 @@ func left_shift_disk_part2(disk []int) []int {
 				continue
 			}
 
-			// lets copy the cell value into the next memorychunk spots
-			for x := j; x < memory_size_chunk+j; x++ {
-				fmt.Println("copying value. Clearing ", disk[x], " over to index ", x, " with value ", cell_value)
-				disk[x] = cell_value
-			}
-
 			// lets clear the memory chunk
-			for x := i - memory_size_chunk; x < i; x-- {
-				fmt.Println("x:", x, " : ", i, " mem_size_chunk:", memory_size_chunk)
+			fmt.Println("first index: ", first_index_of_memory)
+			fmt.Println("i: ", i)
+			for x := first_index_of_memory; x <= i; x++ {
+
+				fmt.Println("x:", x, " : ", i, " mem_size_chunk:", chunk_sizes)
 				fmt.Println("setting value ", disk[x], " to -1 ")
 				disk[x] = -1
+				fmt.Println("disk x =", disk[x])
 			}
 
 			// now lets shift i down mem_size
-			i -= memory_size_chunk
-			break
+			// i -= chunk_sizes
 
+			// lets copy the cell value into the next memorychunk spots
+			for x := j; x < chunk_sizes+j; x++ {
+				fmt.Println("copying value. Clearing ", disk[x], " over to index ", x, " with value ", cell_value)
+				disk[x] = cell_value
+			}
+			fmt.Println("")
+			i = first_index_of_memory + 1
+
+			biggest_or_last_moved = cell_value //update to stop moving smaller chunks of the large one
+			break
 		}
+		// TODO: understand how to always count down how many chunks found
 
 	}
 
